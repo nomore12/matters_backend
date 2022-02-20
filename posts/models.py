a874cc6dacf24edbd19a08f01f165dd9b9df4aed
노성호 <nightwing@naver.com>
2022-02-20 18:42:40 +0900

    포스트 타입 추가

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,16 @@ from django.db import models
 
 # Create your models here.
 
+post_type_choices = [
+    ('residential', 'residential'),
+    ('office', 'office'),
+    ('commercial', 'commercial'),
+    ('hospitality', 'hospitality'),
+    ('exhibition', 'exhibition'),
+    ('furniture', 'furniture'),
+    ('unbuilt', 'unbuilt'),
+    ('etc', 'etc'),
+]
 
 class Post(models.Model):
     title = models.CharField(max_length=128)
@@ -11,7 +21,7 @@ class Post(models.Model):
     description = models.TextField()
     main_image = models.ImageField(blank=True, upload_to="main")
     thumbnail = models.ImageField(blank=True, upload_to="thumbnail")
-    post_type = models.CharField(max_length=32, default="none")
+    post_type = models.CharField(max_length=32, choices=post_type_choices)
 
     # def save(self, *args, **kwargs):
     #     if self.main_image is not None:
