661c17035e9c850b9275444ad9bcdf9602a452fe
노성호 <nightwing@naver.com>
2022-08-16 15:20:10 +0900

    allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-vs0tl*ov)a6-k=0=gq!dm2ff_pjx+jxetcjb%ss!tfzdg^%a7y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', 'http://115.68.232.7/', 'localhost']
+ALLOWED_HOSTS = ['*', 'http://115.68.232.7/', 'localhost', 'http://211.42.153.5/']
 
 
 # Application definition
