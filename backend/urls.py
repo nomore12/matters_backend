60ab29deb20c662503fd017738bf6819220fd0b4
노성호 <nightwing@naver.com>
2022-08-17 13:32:05 +0900

    static url change

--- backend/urls.py
+++ backend/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-    path(r'^administation/static/$', include(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)))
+    # path(r'^administation/static/$', include(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)))
 ]
 
 if settings.DEBUG:
