09428e2e0bd92a0ea2f93cbb4eb9c7f8ba69ae11
노성호 <nightwing@naver.com>
2022-08-29 16:10:07 +0900

    add cors origin whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
 ]
 
 # CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000']
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'http://localhost:8000', 'http://localhost:8080', 'http://211.42.153.34',]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000', 'http://localhost:8000', 'http://localhost:8080', 'http://211.42.153.34', 'http://massnmatters.com']
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'backend.urls'
